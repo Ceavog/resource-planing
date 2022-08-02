dc347c62fe7e7a5365b2b376a7a63233cacc4563
Ceavog <Ceavog13@gmail.com>
2022-08-02 09:56:01 +0200

    added note for idea

--- Backend/Backend.webapi/Program.cs
+++ Backend/Backend.webapi/Program.cs
@@ -8,7 +8,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+//todo - in the future make separate folders for different di 
 builder.Services.AddSingleton<IConnectionStrings>(configuration.GetSection("ConnectionStrings").Get<ConnectionStrings>());
 
 var app = builder.Build();
