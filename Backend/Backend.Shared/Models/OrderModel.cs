7ba9b6e52028147a251ce4fafcd0231d387920fb
Ceavog <Ceavog13@gmail.com>
2022-08-11 13:51:44 +0200

    added shared project

---
+++ Backend/Backend.Shared/Models/OrderModel.cs
@@ -0,0 +1,9 @@
+namespace Shared.Models;
+
+public class OrderModel
+{
+    public int Id { get; set; }
+    public int EmployeeId { get; set; }
+    public DateTime DateTime { get; set; }
+    public int OrderTypeId { get; set; }
+}
