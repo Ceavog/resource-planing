1372fdf3a6d92ef90ac0d223bf69e6ebc8a9d533
Ceavog <Ceavog13@gmail.com>
2022-08-11 10:23:32 +0200

    added basic navbar

--- frontend/src/Components/Navbar.js
+++ frontend/src/Components/Navbar.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import {Link} from 'react-router-dom'
-
+import '../Styles/Navbar.css'
 function Navbar() {
     return (
         <>
-            <nav>
-                <Link to='/Settings'>
-                    Settings
-                </Link>
-                <Link to='/Menu'>
-                    Menu
-                </Link>
-                <Link to='/Orders'>
-                    Orders
-                </Link>
-            </nav>
+                <nav className="navbar navbar-expand-lg navbar-styles">
+                    <Link className="nav-link mx-4 nav-link-hoover" to='/Settings'>
+                        Settings
+                    </Link>
+                    <Link className="nav-link mx-4 nav-link-hoover" to='/Menu'>
+                        Menu
+                    </Link>
+                    <Link className="nav-link mx-4 nav-link-hoover" to='/Orders'>
+                        Orders
+                    </Link>
+                </nav>
         </>
     );
 }
