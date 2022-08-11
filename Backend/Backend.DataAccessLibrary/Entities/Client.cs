64ee1ef23a6f3e367350e2dc5e8235c0a63f87af
Ceavog <Ceavog13@gmail.com>
2022-08-11 14:24:08 +0200

    changed entities so they match database

---
+++ Backend/Backend.DataAccessLibrary/Entities/Client.cs
@@ -0,0 +1,8 @@
+namespace Backend.DataAccessLibrary;
+
+public class Client
+{
+    public int Id { get; set; }
+    public string PhoneNumber { get; set; }
+    public string Address { get; set; }
+}
--- Backend/Backend.DataAccessLibrary/Entities/Delivery.cs
+++ Backend/Backend.DataAccessLibrary/Entities/Delivery.cs
@@ -3,5 +3,5 @@ namespace Backend.DataAccessLibrary;
 public class Delivery{
     public int Id { get; set; }
     public int OrderId { get; set; }
-    public int EmployeeId { get; set; }
+    public int ClientId { get; set; }
 }
