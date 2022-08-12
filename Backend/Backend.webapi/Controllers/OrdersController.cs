91f2ce56f17a45f1554b2ab8b3dbd29932c411db
Ceavog <Ceavog13@gmail.com>
2022-08-12 10:32:38 +0200

    added example controller

---
+++ Backend/Backend.webapi/Controllers/OrdersController.cs
@@ -0,0 +1,21 @@
+using Backend.Services.Interface;
+using Microsoft.AspNetCore.Mvc;
+using Shared.Models;
+
+namespace Backend.webapi;
+
+[ApiController]
+public class OrdersController : Controller
+{
+    private readonly IOrderService _orderService;
+    public OrdersController(IOrderService orderService)
+    {
+        _orderService = orderService;
+    }
+
+    [HttpGet("GetAllOrders")]
+    public ActionResult<IEnumerable<OrderModel>> GetAllOrders()
+    {
+        return Json(_orderService.GetAllOrders());
+    }
+}
