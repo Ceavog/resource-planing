5a21f6bdcb017a92959a5dcd75977cc70c420cd5
Ceavog <Ceavog13@gmail.com>
2022-08-12 10:25:47 +0200

    final UnitOfWork form?

--- Backend/Backend.DataAccessLibrary/UnitOfWork/UnitOfWork.cs
+++ Backend/Backend.DataAccessLibrary/UnitOfWork/UnitOfWork.cs
@@ -4,20 +4,24 @@ namespace Backend.DataAccessLibrary.UnitOfWork;
 
 public class UnitOfWork: IUnitOfWork, IDisposable
 {
+    private readonly IConnectionFactory _connectionFactory;
     private readonly IDbTransaction _dbTransaction;
-    public UnitOfWork(IDbTransaction dbTransaction)
+    public UnitOfWork(IConnectionFactory connectionFactory)
     {
-        _dbTransaction = dbTransaction;
+        _connectionFactory = connectionFactory;
+        _dbTransaction = _connectionFactory.GetConnection.BeginTransaction();
     }
-    
-    public IGenericRepository<Category> _CategoryRepository { get; }
-    public IGenericRepository<Client> _ClientRepository { get; }
-    public IGenericRepository<Delivery> _DeliveryRepository { get; }
-    public IGenericRepository<Employee> _EmployeeRepository { get; }
-    public IGenericRepository<MenuPosition> _MenuPositionRepository { get; }
-    public IGenericRepository<Order> _OrderRepository { get; }
-    public IGenericRepository<OrderPosition> _OrderPositionRepository { get; }
-    public IGenericRepository<OrderType> _OrderTypeRepository { get; }
+
+    public IGenericRepository<Category> _CategoryRepository => new GenericRepository<Category>(_dbTransaction);
+    public IGenericRepository<Client> _ClientRepository => new GenericRepository<Client>(_dbTransaction);
+    public IGenericRepository<Delivery> _DeliveryRepository => new GenericRepository<Delivery>(_dbTransaction);
+    public IGenericRepository<Employee> _EmployeeRepository => new GenericRepository<Employee>(_dbTransaction);
+    public IGenericRepository<MenuPosition> _MenuPositionRepository =>
+        new GenericRepository<MenuPosition>(_dbTransaction);
+    public IGenericRepository<Order> _OrderRepository => new GenericRepository<Order>(_dbTransaction);
+    public IGenericRepository<OrderPosition> _OrderPositionRepository =>
+        new GenericRepository<OrderPosition>(_dbTransaction);
+    public IGenericRepository<OrderType> _OrderTypeRepository => new GenericRepository<OrderType>(_dbTransaction);
     public void Rollback()
     {
         _dbTransaction.Rollback();
