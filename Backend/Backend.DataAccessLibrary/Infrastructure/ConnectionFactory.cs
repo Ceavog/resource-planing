198493be9001aaa70ce583234da047ec2feaf8de
Ceavog <Ceavog13@gmail.com>
2022-08-12 11:00:38 +0200

    now ConnectionFactory use connection string from appsettings

--- Backend/Backend.DataAccessLibrary/Infrastructure/ConnectionFactory.cs
+++ Backend/Backend.DataAccessLibrary/Infrastructure/ConnectionFactory.cs
@@ -1,4 +1,5 @@
 using System.Data;
+using Backend.DataAccessLibrary.Configuration;
 using MySql.Data.MySqlClient;
 
 namespace Backend.DataAccessLibrary;
@@ -6,21 +7,21 @@ namespace Backend.DataAccessLibrary;
 
 public class ConnectionFactory : IConnectionFactory
 {
-    // todo - make connection factory use connection string form appsettings.json
-    // private readonly IConnectionStrings _connectionStrings;
-    // public ConnectionFactory(IConnectionStrings connectionStrings)
-    // {
-    //     this._connectionStrings = connectionStrings;
-    // }
-    
+    private readonly IConnectionStrings _connectionStrings;
+    public ConnectionFactory(IConnectionStrings connectionStrings)
+    {
+        this._connectionStrings = connectionStrings;
+    }
     
      public IDbConnection GetConnection
     {
         get
         {
-            //connection.ConnectionString = _connectionStrings.Sample_Database;
+            
             var connection = new MySqlConnection(); 
-            connection.ConnectionString = "Server=localhost; Port=6603; Database=et_database; Uid=root; Pwd=pieczywo;";
+            //for testing
+            // connection.ConnectionString = "Server=localhost; Port=6603; Database=et_database; Uid=root; Pwd=pieczywo;";
+            connection.ConnectionString = _connectionStrings.Sample_Database;
             connection.Open();
             return connection;
         }
