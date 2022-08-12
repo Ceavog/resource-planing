1ffe70772bc6f4e7e0937e507747982728564b41
Ceavog <Ceavog13@gmail.com>
2022-08-12 10:21:19 +0200

    minor chagne in ConnectionFactory

--- Backend/Backend.DataAccessLibrary/Infrastructure/ConnectionFactory.cs
+++ Backend/Backend.DataAccessLibrary/Infrastructure/ConnectionFactory.cs
@@ -4,7 +4,7 @@ using MySql.Data.MySqlClient;
 namespace Backend.DataAccessLibrary;
 
 
-public abstract class ConnectionFactory : IConnectionFactory
+public class ConnectionFactory : IConnectionFactory
 {
     // todo - make connection factory use connection string form appsettings.json
     // private readonly IConnectionStrings _connectionStrings;
@@ -18,16 +18,7 @@ public abstract class ConnectionFactory : IConnectionFactory
     {
         get
         {
-            // todo - change to use factory insted of simple connection - whatever does it mean 
-            
-            // DbProviderFactories.RegisterFactory("System.Data.SqlClient", System.Data.SqlClient.SqlClientFactory.Instance);
-            // var factory = DbProviderFactories.GetFactory("System.Data.SqlClient");
-            // var connection = factory.CreateConnection();
-            
             //connection.ConnectionString = _connectionStrings.Sample_Database;
-            //for now its hardcoded because i cant find i way to test this
-            // todo -- maybe ask someone more experienced
-
             var connection = new MySqlConnection(); 
             connection.ConnectionString = "Server=localhost; Port=6603; Database=et_database; Uid=root; Pwd=pieczywo;";
             connection.Open();
