e84f5ea5bc3bd1eec8b2729d77b42ef20c4d6cff
Ceavog <Ceavog13@gmail.com>
2022-08-16 09:15:52 +0200

    added cunstom repository

--- Backend/Backend.DataAccessLibrary/Repositories/CustomRepositiories/CustomRepository.cs
+++ Backend/Backend.DataAccessLibrary/Repositories/CustomRepositiories/CustomRepository.cs
@@ -1,10 +1,35 @@
+using System.Data;
+using Dapper;
+
 namespace Backend.DataAccessLibrary.Repositories;
 
 public class CustomRepository : ICustomRepository
 {
-    private readonly IConnectionFactory _connectionFactory;
-    public CustomRepository(IConnectionFactory connectionFactory)
+    private readonly IDbTransaction _dbTransaction;
+    public CustomRepository(IDbTransaction dbTransaction)
     {
-        _connectionFactory = connectionFactory;
+        _dbTransaction = dbTransaction;
     }
+
+    public IEnumerable<Order> GetOrdersWithPositions(string condition)
+    {
+       var sql = File.ReadAllText("./CustomSqlQueries/GetAllPositions.sql");
+        sql = sql.Replace("condition", condition);
+        var orders = _dbTransaction.Connection.Query<Order, MenuPosition, Order>(sql,
+            (order, menuPosition) =>
+            {
+                order.MenuPositions = new List<MenuPosition> { menuPosition };
+                return order;
+            }, splitOn:"MenuPositionId");
+
+        var result = orders.GroupBy(x => x.Id).Select(y =>
+        {
+            var groupedOrder = y.First();
+            groupedOrder.MenuPositions = y.Select(x => x.MenuPositions.Single()).ToList();
+            return groupedOrder;
+        }).ToList();
+
+        return result;
+    }
+    
 }
--- Backend/Backend.DataAccessLibrary/Repositories/CustomRepositiories/ICustomRepository.cs
+++ Backend/Backend.DataAccessLibrary/Repositories/CustomRepositiories/ICustomRepository.cs
@@ -2,5 +2,5 @@ namespace Backend.DataAccessLibrary.Repositories;
 
 public interface ICustomRepository
 {
-    
+    IEnumerable<Order> GetOrdersWithPositions(string condition);
 }
