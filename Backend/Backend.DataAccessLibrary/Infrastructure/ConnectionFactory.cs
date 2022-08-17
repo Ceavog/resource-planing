a4faeb53a879b7e0f35d6c12705364a1427d1086
Ceavog <Ceavog13@gmail.com>
2022-08-17 13:55:10 +0200

    added some documentation

--- Backend/Backend.DataAccessLibrary/Infrastructure/ConnectionFactory.cs
+++ Backend/Backend.DataAccessLibrary/Infrastructure/ConnectionFactory.cs
@@ -7,8 +7,9 @@ namespace Backend.DataAccessLibrary;
 
 public class ConnectionFactory : IConnectionFactory
 {
-    private readonly IConnectionStrings _connectionStrings;
-    public ConnectionFactory(IConnectionStrings connectionStrings)
+    //constructor must be commented out for testing
+    private readonly IConfiguration _connectionStrings;
+    public ConnectionFactory(IConfiguration connectionStrings)
     {
         this._connectionStrings = connectionStrings;
     }
@@ -19,8 +20,8 @@ public class ConnectionFactory : IConnectionFactory
         {
             
             var connection = new MySqlConnection(); 
-            //for testing
-            // connection.ConnectionString = "Server=localhost; Port=6603; Database=et_database; Uid=root; Pwd=pieczywo;";
+            // for testing
+            //connection.ConnectionString = "Server=localhost; Port=6603; Database=et_database; Uid=root; Pwd=pieczywo;";
             connection.ConnectionString = _connectionStrings.Sample_Database;
             connection.Open();
             return connection;
