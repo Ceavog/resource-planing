da228ca7aa4e0c70e0e4d4138b7605c2fd0488cb
Ceavog <Ceavog13@gmail.com>
2022-08-19 15:25:04 +0200

    changes in model

--- Backend/Backend.DAL_EF/Models/User.cs
+++ Backend/Backend.DAL_EF/Models/User.cs
@@ -3,9 +3,8 @@ namespace Backend.DataAccessLibrary;
 public class User
 {
     public int Id { get; set; }
-    public string Name { get; set; }
-    public string SecondName { get; set; }
+    public string login { get; set; }
+    public string Password { get; set; }
     public int ServicePointId { get; set; }
     public ServicePoint ServicePoint { get; set; }
-    public string test { get; set; }
 }
