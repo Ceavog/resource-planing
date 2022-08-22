34c8d85547b5d35e0ca9ba5ebb143b9be12f39df
Ceavog <Ceavog13@gmail.com>
2022-08-22 09:51:44 +0200

    register method in UserService

--- Backend/Backend.Services/Interface/IUserService.cs
+++ Backend/Backend.Services/Interface/IUserService.cs
@@ -1,7 +1,9 @@
+using Backend.DataAccessLibrary;
+
 namespace Backend.Services.Interface;
 
 public interface IUserService
 {
-    long RegisterUser(string login, string password);
+    User RegisterUser(string login, string password);
     void Login(string login, string password);
 }
--- Backend/Backend.Services/Services/UserService.cs
+++ Backend/Backend.Services/Services/UserService.cs
@@ -1,3 +1,4 @@
+using Backend.DAL_EF;
 using Backend.DataAccessLibrary;
 using Backend.Services.Interface;
 using BCrypt.Net;
@@ -6,20 +7,31 @@ namespace Backend.Services.Services;
 
 public class UserService : IUserService
 {
-    public long RegisterUser(string login, string password)
+    private readonly ApplicationDbContext _applicationDbContext;
+    public UserService(ApplicationDbContext applicationDbContext)
     {
-        // password = BCrypt.Net.BCrypt.HashPassword(password);        
-        // var user = new ServicePoint()
-        // {
-        //     Login = login,
-        //     Password = password
-        // };
-        // var id = _unitOfWork._UserRepository.Add(user);
-        // _unitOfWork.Commit();
-        // _unitOfWork.Dispose();
-        // return id;
-        throw new NotImplementedException();
+        _applicationDbContext = applicationDbContext;
     }
+    /// <summary>
+    /// this method adds User with default service point 
+    /// </summary>
+    /// <param name="login"></param>
+    /// <param name="password"></param>
+    /// <returns></returns>
+    public User RegisterUser(string login, string password)
+    {
+        password = BCrypt.Net.BCrypt.HashPassword(password);
+        var user = new User()
+        {
+            login = login,
+            Password = password,
+            ServicePointId = 1
+        };
+        var addedUser =_applicationDbContext.Users.Add(user).Entity;
+        _applicationDbContext.SaveChanges();
+        return addedUser;
+    }
+
 
     public void Login(string login, string password)
     {
