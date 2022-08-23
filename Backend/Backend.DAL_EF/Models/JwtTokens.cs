f56bf132451f473dd9a9f063b71e3b2a6ea1be10
Ceavog <Ceavog13@gmail.com>
2022-08-23 10:32:06 +0200

    added jwt token to database

---
+++ Backend/Backend.DAL_EF/Models/JwtTokens.cs
@@ -0,0 +1,12 @@
+using System.Security.Cryptography;
+
+namespace Backend.DataAccessLibrary;
+
+public class JwtTokens
+{
+    public int Id { get; set; }
+    public string JwtToken { get; set; }
+    
+    public int UserId { get; set; }
+    public User User { get; set; }
+}
