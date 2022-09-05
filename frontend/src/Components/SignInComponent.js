8609a722a22060e81bb7890317060e4b9b42c07b
Ceavog <Ceavog13@gmail.com>
2022-09-05 16:45:43 +0200

    added validation to login and register component

--- frontend/src/Components/SignInComponent.js
+++ frontend/src/Components/SignInComponent.js
@@ -1,25 +1,85 @@
-import React from "react";
+import React, {useState} from "react";
 import Button from "bootstrap/js/src/button";
 
 function SignInComponent(){
+    const [login, setLogin] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+
+    const [loginMessage, setLoginMessage] = useState('');
+    const [PasswordMessage, setPasswordMessage] = useState('');
+
+    const [isValidForSubmit, setIsValidForSubmit] = useState(true)
+
+    function validateField(){
+        let isPasswordValid = false;
+        let isLoginValid = false;
+        if (password.length < 1){
+            setPasswordMessage('password is too short')
+            isPasswordValid = false
+        }
+        else{
+            setPasswordMessage('');
+            isPasswordValid = true
+        }
+
+        if(login.length < 1){
+            setLoginMessage("login is too short")
+            isLoginValid = false
+        }
+        else{
+            setLoginMessage('');
+            isLoginValid = true
+        }
+
+        if(isLoginValid && isLoginValid){
+            setIsValidForSubmit(true)
+        }
+        else{
+            setIsValidForSubmit(false)
+        }
+
+
+    }
+
     return(
         <div>
             <form className={"mt-5"}>
                 <div className={"form-outline mb-4"}>
-                    <label className={"form-label"}>Login</label>
-                    <input className={"form-control"} type={"text"} name={"Name"}/>
+                    <input
+                        placeholder='Login'
+                        className={"form-control"}
+                        type={"text"}
+                        name={"Name"}
+                        value={login}
+                        onChange={(e) => setLogin(e.target.value)}
+                        onBlur={(e) => validateField()}
+                    />
                 </div>
+
                 <div className={"form-outline mb-4"}>
-                    <label className={"form-label"}>Password</label>
-                    <input className={"form-control"} type={"text"} name={"Name"}/>
-                    <a href="#!">Forgot password?</a>
+                    <input
+
+                        placeholder="Password"
+                        className={"form-control"}
+                        type={"password"}
+                        name={"Name"}
+                        value={password}
+                        onChange={(e)=> setPassword(e.target.value)}
+                        onBlur={(e) => validateField()}
+
+                    />
                 </div>
+
+                <h6 className={"text-danger"}>{PasswordMessage}</h6>
+                <h6 className={"text-danger"} >{loginMessage}</h6>
                 <div className={"d-flex justify-content-center mb-3"}>
-                    <input className={"btn btn-primary"} type={"submit"} value={"Sign In"}/>
+                    <input className={"btn btn-primary"} type={"submit"} value={"Sign Up"} disabled={!isValidForSubmit}/>
                 </div>
             </form>
         </div>
     );
+
 }
 
 export default SignInComponent;
