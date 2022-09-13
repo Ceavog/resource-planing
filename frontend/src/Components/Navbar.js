988605e37d072afe89e17c5ab38e75cfa01477d1
Ceavog <Ceavog13@gmail.com>
2022-09-13 08:31:05 +0200

    pages for users

--- frontend/src/Components/Navbar.js
+++ frontend/src/Components/Navbar.js
@@ -32,15 +32,19 @@ function Navbar() {
     return (
         <>
                 <nav className="navbar navbar-expand-lg navbar-styles d-flex">
-                    <Link className="nav-link mx-4 nav-link-hoover" to='/Settings'>
-                    Settings
-                    </Link>
-                    <Link className="nav-link mx-4 nav-link-hoover" to='/Menu'>
-                    Menu
-                    </Link>
-                    <Link className="nav-link mx-4 nav-link-hoover" to='/Orders'>
-                    Orders
-                    </Link>
+                        {cookies.jwt !== undefined &&
+                            <>
+                                <Link className="nav-link mx-4 nav-link-hoover" to='/Settings'>
+                                    Settings
+                                </Link>
+                                <Link className="nav-link mx-4 nav-link-hoover" to='/Menu'>
+                                    Menu
+                                </Link>
+                                <Link className="nav-link mx-4 nav-link-hoover" to='/Orders'>
+                                    Orders
+                                </Link>
+                            </>
+                        }
                     <Link className="nav-link mx-4 nav-link-hoover ms-auto " to='/SignInUp'>
                         <>
                             <Dropdown>
