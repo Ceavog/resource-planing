d002c96db17d840a69bcfb468e32b69e5b429215
Ceavog <Ceavog13@gmail.com>
2022-10-17 14:01:55 +0200

    modified exampleController so it can be example

--- Backend/Backend.webapi/Controllers/ExampleController.cs
+++ Backend/Backend.webapi/Controllers/ExampleController.cs
@@ -7,18 +7,6 @@ namespace Backend.webapi;
 [ApiController]
 public class ExampleController : Controller
 {
-    private readonly ICategoryService _categoryService;
-
-    public ExampleController(ICategoryService categoryService)
-    {
-        _categoryService = categoryService;
-    }
-    [HttpGet("example")]
-    public IActionResult GetSampleString()
-    {
-        return Ok(_categoryService.GetTypeName_(1));
-    }
-    
     [HttpGet("example1")]
     public IActionResult GetSampleString1()
     {
