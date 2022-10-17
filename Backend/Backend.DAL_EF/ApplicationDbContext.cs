e5d8420f6d61d12eb4495d6eceb53a296e77a53e
Ceavog <Ceavog13@gmail.com>
2022-10-17 16:11:00 +0200

    now all dbsets are virtual

--- Backend/Backend.DAL_EF/ApplicationDbContext.cs
+++ Backend/Backend.DAL_EF/ApplicationDbContext.cs
@@ -17,11 +17,11 @@ public class ApplicationDbContext : DbContext
 //    public DbSet<Client> Clients { get; set; }
 //    public DbSet<Delivery> Deliveries { get; set; }
 //    public DbSet<Employee> Employees { get; set; }
-    public DbSet<Products> Products { get; set; }
-    public DbSet<Order> Orders { get; set; }
-    public DbSet<OrderPosition> OrderPositions { get; set; }
-    public DbSet<OrderType> OrderTypes { get; set; }
+    public virtual DbSet<Products> Products { get; set; }
+    public virtual DbSet<Order> Orders { get; set; }
+    public virtual DbSet<OrderPosition> OrderPositions { get; set; }
+    public virtual DbSet<OrderType> OrderTypes { get; set; }
 //    public DbSet<ServicePoint> ServicePoints { get; set; }
-    public DbSet<User> Users { get; set; } 
+    public virtual DbSet<User> Users { get; set; } 
 //    public DbSet<CategoryPositions> CategoryPositions { get; set; }
 }
