fc85b5fc24fd0792ee16aef36030a0645bbf98c4
Ceavog <Ceavog13@gmail.com>
2022-10-17 15:34:27 +0200

    deleted order related parts

--- Backend/Backend.Services/Interface/IOrderService.cs
+++
@@ -1,9 +0,0 @@
-using Backend.DataAccessLibrary;
-
-namespace Backend.Services.Interface;
-
-public interface IOrderService
-{
-    IEnumerable<Order> GetAllOrders();
-    IEnumerable<Order> GetAllOrdersWithOrderPositions();
-}
