c8a7f39ed122b41099e61403d5557d371d36cf81
ceavog <ceavog13@gmail.com>
2022-12-01 18:30:40 +0100

    added id and deleted userId from updateProductDto

--- Backend/Backend.Shared/Dtos/ProductDtos/UpdateProductDto.cs
+++ Backend/Backend.Shared/Dtos/ProductDtos/UpdateProductDto.cs
@@ -2,8 +2,8 @@ namespace Backend.Shared.Dtos.ProductDtos;
 
 public class UpdateProductDto
 {
+    public int Id { get; set; }
     public string Name { get; set; }
     public double Price { get; set; }
     public string Section { get; set; }
-    public int UserId { get; set; }
 }
