daf26a669bbf914b0d03b17f71b47f9e847c422f
ceavog <ceavog13@gmail.com>
2022-12-02 15:56:30 +0100

    added UserId to database

--- Backend/Backend.DAL_EF/Models/Category.cs
+++ Backend/Backend.DAL_EF/Models/Category.cs
@@ -3,5 +3,7 @@ namespace Backend.DataAccessLibrary;
 public class Category{
     public int Id { get; set; }
     public string Name { get; set; }
+    public int UserId { get; set; }
+    public User User { get; set; }
     
 }
