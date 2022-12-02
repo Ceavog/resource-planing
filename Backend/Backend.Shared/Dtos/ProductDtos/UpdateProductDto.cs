489a16ec0e322374f230ab66c266e698dc6ff928
ceavog <ceavog13@gmail.com>
2022-12-02 14:03:57 +0100

    added userId to UpdateProductDto

--- Backend/Backend.Shared/Dtos/ProductDtos/UpdateProductDto.cs
+++ Backend/Backend.Shared/Dtos/ProductDtos/UpdateProductDto.cs
@@ -6,4 +6,5 @@ public class UpdateProductDto
     public string Name { get; set; }
     public double Price { get; set; }
     public string Section { get; set; }
+    public int UserId { get; set; }
 }
