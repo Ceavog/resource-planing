017fb2ad503071b7e38fa7e1fc0dd6b7128d4f67
ceavog <ceavog13@gmail.com>
2022-12-06 14:58:49 +0100

    Added exception

--- Backend/Backend.Shared/Exceptions/CategoryExceptions/CategoryWithGivenNameAndUserWithThisIdAlreadyExistsException.cs
+++ Backend/Backend.Shared/Exceptions/CategoryExceptions/CategoryWithGivenNameAndUserWithThisIdAlreadyExistsException.cs
@@ -1,6 +1,14 @@
 namespace Backend.Shared.Exceptions.CategoryExceptions;
 
-public class CategoryWithGivenNameAndUserWithThisIdAlreadyExistsException
+public class CategoryWithGivenNameAndUserWithThisIdAlreadyExistsException : Exception
 {
-    
+    public int UserId { get; set; }
+    public string CategoryName { get; set; }
+
+    public CategoryWithGivenNameAndUserWithThisIdAlreadyExistsException(string categoryName, int userId) 
+        : base($"category with given name: {categoryName} already exists for user with id: {userId}")
+    {
+        UserId = userId;
+        CategoryName = categoryName;
+    }
 }
