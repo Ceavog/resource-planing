6053e0471902ccbeee8486daf560d74cb9404ae6
ceavog <ceavog13@gmail.com>
2022-12-07 14:58:00 +0100

    deleted duplicated dto

--- Backend/Backend.Shared/Dtos/CategoryDtos/GetCategoryWithProductsDto.cs
+++
@@ -1,9 +0,0 @@
-using Backend.Shared.Dtos.ProductDtos;
-
-namespace Backend.Shared.Dtos.CategoryDtos;
-
-public class GetCategoryWithProductsDto
-{
-    public CategoryDto Category { get; set; }
-    public IEnumerable<ProductDto> Products { get; set; }
-}
